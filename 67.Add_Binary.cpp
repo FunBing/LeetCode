204efdee08fcbb890a9a798fcd391ab679528f38
FunBing <316840308@qq.com>
2017-05-30 21:10:34 +0800

    Q 67 renewed

--- 67.Add_Binary.cpp
+++ 67.Add_Binary.cpp
@@ -1,50 +1,22 @@
-//This is my own code
+//After I saw the solution
 class Solution {
 public:
     string addBinary(string a, string b) {
-        if(a.size() > b.size())
-            return addBinary(b, a);
-
-        //所有a的长度 <= b的长度
-        int lengthA = a.size(), lengthB = b.size();
-
-        for(int i = 1; i <= lengthA; ++i)
+        int posA = a.size() - 1, posB = b.size() - 1;
+        int carry = 0;
+        int count = 0;
+        string result;
+        while(posA >= 0 || posB >= 0 || carry == 1)
         {
-            b[lengthB - i] += a[lengthA - i] - '0';
-            if(b[lengthB - i] >= '2')
-            {
-                if(lengthB - i == 0)
-                {
-                    b[0] -= 2;
-                    b.insert(b.begin(), '1');
-
-                    return b;
-                }
-
-                b[lengthB - i - 1] += 1;
-                b[lengthB - i] -= 2;
-            }
-
+            count = 0;
+            if(posA >= 0)count += a[posA--] - '0';
+            if(posB >= 0)count += b[posB--] - '0';
+            count += carry;
+            result = to_string(count % 2) + result;
+            carry = count / 2;
         }
 
-        for(int i = lengthB - lengthA - 1; i > 0; --i)
-        {
-            if(b[i] >= '2')
-            {
-                b[i] -= 2;
-                b[i - 1] +=1 ;
-            }
-        }
-
-        if(b[0] < '2')
-            return b;
-        else
-        {
-            b[0] -= 2;
-            b.insert(b.begin(), '1');
-        }
-
-        return b;
+        return result;
 
 
     }
